fa622a467ed726866c6cd969da867c6331b92685
VOJTECH_KANIA <vojtech.kania@gmail.com>
2023-01-15 10:15:46 +0100

    edited asyncio

--- description_download_async.py
+++ description_download_async.py
@@ -5,13 +5,67 @@ import numpy as np
 
 file_name="Prague_apartments_sale_2023-01-14.gzip"
 
-df=pd.read_parquet(file_name, columns=["hash_id"])
-df.head()
+desc_indices=pd.read_parquet(file_name, columns=["hash_id"])
+desc_indices=desc_indices.drop_duplicates()
 
-df=df.head(10)
-df.shape
+urls=["https://www.sreality.cz/api/cs/v2/estates/"+str(i) for i in desc_indices["hash_id"]]
 
-urls=["https://www.sreality.cz/api/cs/v2/estates/"+str(i) for i in df["hash_id"]]
+items_dict={"Zlevněno":"discounted", 
+"Původní cena":"price_original", 
+"ID zakázky":"id_order", 
+"Aktualizace":"date_update", 
+"Stavba":"building_type", 
+"Stav objektu":"building_condition", 
+"Umístění objektu":"building_location", 
+"Užitná plocha":"area_net", 
+"Plocha podlahová":"area_floor", 
+"Celková cena":"price_total", 
+"Poznámka k ceně":"price_note", 
+"Doprava":"transport", 
+"Plocha zastavěná":"area_build_up", 
+"Datum nastěhování":"date_moving_in", 
+"Rok kolaudace":"date_building_approval", 
+"Vybavení":"equipped", 
+"Elektřina":"electricity", 
+"Komunikace":"traffic_communication", 
+"Bezbariérový":"barrier_free", 
+"ID":"id", 
+#"Cena":"price", 
+"Energetická náročnost budovy":"energy_efficient_rating", 
+"Výtah":"lift", 
+"Typ domu":"building_floor_type", 
+"Podlaží":"building_floor", 
+"Voda":"water", 
+"Plyn":"gas", 
+"Odpad":"waste", 
+"Topení":"heating", 
+"Telekomunikace":"telecommunication", 
+"Rok rekonstrukce":"date_year_reconstruction", 
+"Průkaz energetické náročnosti budovy":"energy_efficient_rating_card", 
+"Stav":"availability", 
+"Datum zahájení prodeje":"date_offer_start", 
+"Ukazatel energetické náročnosti budovy":"energy_efficient_rating_index", 
+"Datum prohlídky":"date_tour", 
+"Datum prohlídky do":"date_tour_to", 
+"Plocha zahrady":"area_garden",
+"Výška stropu":"ceiling_height",
+"Parkování":"parking",
+"Cena za m²":"price_sqm",
+"Počet kanceláří":"office_count",
+"Garáž":"garage",
+"Počet míst":"place_count",
+"Bazén":"basin",
+"Plocha pozemku":"area_landplot",
+"Velikost podílu":"share_size",
+"Vyvolávací cena":"auction_initial_price",
+"Znalecký posudek":"expert_opinion_official",
+"Minimální příhoz":"auction_min_bid", 
+"Aukční jistina":"auction_principal",
+"Druh dražby":"auction_type",
+"Místo konání dražby":"auction_place",
+"Datum konání dražby":"auction_date",
+"Dražební vyhláška":"auction_decree",
+"Posudek znalce":"expert_opinion"}
 
 async def get_response(session, url):
     try:
@@ -30,13 +84,14 @@ async def main(urls, chunk_size):
             print(f'running chunk {chunk_idx+1} out of {len(chunks)} chunks ({chunk_size} items)')
             #here you process first batch -> request go async
 
-            tasks = [get_response(session, url) for url in urls]
+            tasks = [get_response(session, url) for url in chunk]
             #here they come together
             responses = await asyncio.gather(*tasks)
             
             #responses can be saved -> csv, sqlite etc
-            all_responses.append(responses)
+            all_responses=all_responses+responses
         return all_responses
 
-loop = asyncio.get_event_loop()
+loop=asyncio.new_event_loop()
 my_resp=loop.run_until_complete(main(urls, 5))
+loop.close()
