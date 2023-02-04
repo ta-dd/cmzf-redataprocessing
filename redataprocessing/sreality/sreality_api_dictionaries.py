8b3ffc9b8272c82e6dcdfb752ea5e29fbc5c330f
luky1400 <99750771+luky1400@users.noreply.github.com>
2023-02-04 14:08:03 +0100

    Adding locality_region_ids

--- redataprocessing/sreality/sreality_api_dictionaries.py
+++ redataprocessing/sreality/sreality_api_dictionaries.py
@@ -37,7 +37,22 @@ def db_table_names_type():
     return(dict)
 
 def locality_region_id_dict():
-    dict={10:"Praha", 11:"Středočeský kraj", 5:"Liberecký kraj"}
+    dict={
+        1:"Jihočeský",
+        2:"Plzeňský",
+        3:"Karlovarský",
+        4:"Ústecký",
+        5:"Liberecký",
+        6:"Královéhradecký",
+        7:"Pardubický",
+        8:"Olomoucký",
+        9:"Zlínský",
+        10:"Praha",
+        11:"Středočeský kraj",
+        12:"Moravskoslezský",
+        13:"Vysočina",
+        14:"Jihomoravský"
+        }
     #locality_region_id_dict=pd.DataFrame.from_dict(locality_region_id_dict, orient="index", columns=["locality_region_id"])
     #print(locality_region_id_dict)
     return(dict)
