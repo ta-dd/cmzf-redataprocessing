83b19b5d62ef860b3fd1992b2f9c5cd01bc6638c
VOJTECH_KANIA <vojtech.kania@gmail.com>
2023-02-04 10:47:17 +0100

    making it installable

--- redataprocessing/setup.py
+++ redataprocessing/setup.py
@@ -1,23 +1,8 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
-DESCRIPTION = 'A download of real estate offers from Sreality.cz'
+find_packages()
 
-setup(
-    name="redataprocessing",
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=open("README.txt").read(),
-    author="Vojtěch Kania, Lukáš Novotný",
-    author_email="vojtech.kania@gmail.com, 30702889@fsv.cuni.cz",
-    license='MIT',
-    packages=find_packages(),
-    install_requires=["python-certifi-win32"],
-    keywords='real estate',
-    classifiers= [
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: MIT License',
-        "Programming Language :: Python :: 3",
-    ]
-)
+if __name__ == "__main__":
+    setup()
--- trying_our_package.py
+++ trying_our_package.py
@@ -1,3 +1,21 @@
 
-import sreality
-help(sreality)
+import redataprocessing as rdp
+
+from inspect import getmembers, isfunction
+
+print(getmembers(rdp.sreality, isfunction))
+
+import imp
+import os
+MODULE_EXTENSIONS = ('.py', '.pyc', '.pyo')
+
+def package_contents(package_name):
+    file, pathname, description = imp.find_module(package_name)
+    if file:
+        raise ImportError('Not a package: %r', package_name)
+    # Use a set because some may be both source and compiled.
+    return set([os.path.splitext(module)[0]
+        for module in os.listdir(pathname)
+        if module.endswith(MODULE_EXTENSIONS)])
+
+package_contents("rdp.sreality")
